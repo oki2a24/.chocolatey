2cf9b1874cd6c280d95830d821029b830539b096
oki2a24 <oki2a24@gmail.com>
2019-01-29 07:48:57 +0900

    fix: リポジトリがすでに存在する場合に発生するエラーに対処する

--- download_install.ps1
+++ download_install.ps1
@@ -5,7 +5,10 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install -y git
 
 # .chocolatey リポジトリの取得
-C:\Program` Files\Git\cmd\git.exe clone https://github.com/oki2a24/.chocolatey.git
+if (!(Test-Path .\.chocolatey))
+{
+    C:\Program` Files\Git\cmd\git.exe clone https://github.com/oki2a24/.chocolatey.git
+}
 
 # インストールスクリプトの実行
 .\.chocolatey\install.ps1
