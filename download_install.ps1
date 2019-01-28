27d2c73e36eea60b8c6723cb873820510798dbd2
oki2a24 <oki2a24@gmail.com>
2019-01-29 07:42:16 +0900

    refactor: 余分なコメントを除去する

--- download_install.ps1
+++ download_install.ps1
@@ -1,9 +1,4 @@
-﻿# Set-ExecutionPolicy Bypass -Scope Process -Force; .\install.ps1
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/oki2a24/.chocolatey/master/bin/download_install.ps1'))
-# ダウンロード例
-# Invoke-WebRequest -Uri https://github.com/oki2a24/.chocolatey/archive/master.zip -OutFile chocolatey.zip
-
-# Chocolatey のインストール
+﻿# Chocolatey のインストール
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Git のインストール
